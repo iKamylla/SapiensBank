4af15065140a5e7c0b6577b489f7b280c3a127e5
Kamylla Rodrigues <115313196+iKamylla@users.noreply.github.com>
2025-11-19 19:55:26 -0400

    Refactor Program class to include title method
    Added a method to create a title and display a closing message.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
-﻿var banco = new Banco();
-var ux = new UX("Sapiens Bank", banco);
-ux.Executar();
+using System;
+using static System.Console;
+
+// Classe Program que contém o main
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var banco = new Banco();
+        var ux = new UX("Sapiens Bank", banco);
+        ux.Executar();
+
+        CriarTitulo("Obrigado por usar o Sistema!");
+        WriteLine(" O programa será encerrado.");
+    }
+
+    // Método auxiliar para Program (para uma saída limpa)
+    private static void CriarTitulo(string titulo)
+    {
+        Clear();
+        ForegroundColor = ConsoleColor.White;
+        WriteLine("-------------------------------------------------");
+        ForegroundColor = ConsoleColor.Yellow;
+        WriteLine(" " + titulo);
+        ForegroundColor = ConsoleColor.White;
+        WriteLine("-------------------------------------------------");
+    }
+}
